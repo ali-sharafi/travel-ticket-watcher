afb63d71b8eae2db701bec1b3b30e55594f06887
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-05 22:42:01 +0330

    Add user model

---
+++ src/models/user.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../utils/database';
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+export default User;
