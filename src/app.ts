b91a36794dcae308f36f9c196261c7af5b685097
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-06 21:29:30 +0330

    Add body parser

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import index from './routes';
 import sequelize from './utils/database';
+import bodyParser from 'body-parser';
+
 
 if (process.env.APP_MODE !== 'prod')
-    sequelize.sync();
+sequelize.sync();
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use('/api', index);
 
@@ -16,4 +19,5 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     else next();
 })
 
+console.info('App is running on port 3000...');
 app.listen(3000);
