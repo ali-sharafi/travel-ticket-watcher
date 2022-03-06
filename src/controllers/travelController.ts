c29c900619089ad3e2a6af74029cd69901d6d06d
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-06 21:29:54 +0330

    Complete add travel

--- src/controllers/travelController.ts
+++ src/controllers/travelController.ts
@@ -1,12 +1,21 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
+import Travel from "../models/travel";
 
 export class TravelController {
     constructor() {
 
     }
 
-    static handleAdd(req: Request, res: Response, next: NextFunction) {
+    static handleAdd(req: Request, res: Response) {
+        Travel.create({
+            type: req.body.type,
+            origin: req.body.origin,
+            destination: req.body.destination,
+            date_at: req.body.date_at,
+            max_price: req.body.max_price ?? null
+        });
 
+        res.send('success');
     }
 
     add(from: Number, to: Number, date: Date) {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,22 @@
-import Router from 'express';
+import Router, { Request, Response } from 'express';
 import { TravelController } from '../controllers/travelController';
+import { body, validationResult } from 'express-validator';
+import { TravelTypes } from '../utils/enums';
 
 const router = Router();
 
-router.post('/travel', TravelController.handleAdd);
+router.post('/travel',
+    body('origin').isNumeric(),
+    body('destination').isNumeric(),
+    body('date_at').isDate(),
+    body('type').isIn([TravelTypes.AIRPLAN, TravelTypes.BUS, TravelTypes.TRAIN]),
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        TravelController.handleAdd(req, res);
+    });
 
 export default router;
