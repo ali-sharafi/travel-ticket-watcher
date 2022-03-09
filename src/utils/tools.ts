a27af05a1c8fa10ee2e2c41bee92a7d8986eb242
Ali Sharafi <a.sharafi1993@gmail.com>
2022-03-09 19:22:35 +0330

    Add moment & date tools

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,7 +1,19 @@
+import moment from 'moment';
+
 const sleep: Function = async (millis: number): Promise<void> => {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
 
+const getDate = (): string => {
+    return moment().format('YYYY-MM-DD');
+}
+
+const getDateTime = (): string => {
+    return moment().format('YYYY-MM-DD HH:mm:ss');
+}
+
 export {
-    sleep
+    sleep,
+    getDate,
+    getDateTime
 }
