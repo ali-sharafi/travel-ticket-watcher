03d13ce35f0899088e228e325fbce5005a8396b5
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-03-08 19:09:27 +0000

    Fix notifications

--- src/sites/alibaba.js
+++ src/sites/alibaba.js
@@ -2,7 +2,8 @@ const { default: axios } = require("axios");
 const logger = require("../utils/logger");
 const { TravelType } = require("../utils/types");
 const telegram = require('../notifications/telegram');
-
+const momentj = require('moment-jalaali');
+const moment = require('moment');
 const BASE_URI = 'https://ws.alibaba.ir/api';
 
 module.exports = async (travel) => {
@@ -26,12 +27,16 @@ async function getAirPlanTravels(travel) {
     if (token) {
         let tickets = await getAirPlanTrips(token);
         if (tickets.length > 0) {
-            let payload = {
-                message: `Airplane Ticket found: ${travel.origin_code} To ${travel.destination_code} for ${travel.date_at}`,
-                link: `https://www.alibaba.ir/flights/${travel.origin_code}-${travel.destination_code}?adult=1&child=0&infant=0&departing=${moment(travel.date_at).format('jYYYY-jMM-jDD')}`
-            }
+            tickets.forEach(ticket => {
+                let payload = {
+                    message: `Airplane Ticket found: ${travel.origin_code} To ${travel.destination_code} for ${ticket.leaveDateTime}`,
+                    link: `https://www.alibaba.ir/flights/${travel.origin_code}-${travel.destination_code}?adult=1&child=0&infant=0&departing=${momentj(travel.date_at).format('jYYYY-jMM-jDD')}`,
+                    travelId: travel.id
+                }
 
-            notify(payload);
+                notify(payload);
+            });
+
         } else logger(`There is not any trips for airplane travel ${travel.origin_code}-${travel.destination_code}:${travel.date_at} at alibaba`, 'alibaba')
     } else logger(`Token not available for airplane travel ${travel.origin_code}-${travel.destination_code}:${travel.date_at} at alibaba`, 'alibaba')
 }
@@ -42,8 +47,9 @@ async function getAirPlanTrips(token) {
             console.log('Some error occured while get alibaba trips: ' + err.message);
         });
 
-    if (res && res.data)
-        return res.data.result.departing;
+    if (res && res.data) {
+        return res.data.result.departing.filter(item => item.seat > 0);
+    }
     return [];
 }
 
@@ -52,7 +58,7 @@ async function getAvailableAirplanToken(travel) {
         adult: 1,
         child: 0,
         infant: 0,
-        departureDate: travel.date_at,
+        departureDate: moment(travel.date_at).format('YYYY-MM-DD'),
         destination: travel.destination_code,
         origin: travel.origin_code
     }).catch((err) => {
@@ -65,8 +71,6 @@ async function getAvailableAirplanToken(travel) {
 }
 
 async function notify(payload) {
-    let message = payload.message + '\n';
-    message += `<a href="${payload.link}">Link</a>`
-    await telegram(message);
+    await telegram(payload.message, payload.link, payload.travelId);
     logger(`Notification sent for ${payload.message}`, 'alibaba');
 }
