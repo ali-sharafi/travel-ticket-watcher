63c99a0e44d6dfb667bd55fd6cae66cfcc000c15
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-03-08 19:10:19 +0000

    Fill cities and register telegram bot

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const dotenv = require('dotenv');
 dotenv.config();
+require('./src/utils/telegramBot')
 const cron = require('node-cron');
 const logger = require('./src/utils/logger');
 const db = require('./src/utils/db');
 const { GetAll } = require('./src/reader');
 const fs = require('fs');
 const City = require('./src/models/City');
+const Travel = require('./src/models/Travel');
+const { TravelType } = require('./src/utils/types');
 
 db.connect().then(() => {
     logger('Going to Update DB');
-    if (City.count() == 0) {
-        fs.readFile('./cities.json', 'utf-8', (err, data) => {
-            if (err) console.log(err);
-            else {
-                let cities = JSON.parse(data);
-                City.insertMany(cities);
-            }
-        })
-    }
+    updateCities();
 })
 
 cron.schedule(process.env.CRON_JOB_SCHEDULE, () => {
@@ -26,3 +21,15 @@ cron.schedule(process.env.CRON_JOB_SCHEDULE, () => {
         logger(e)
     });
 });
+
+async function updateCities() {
+    if ((await City.count()) == 0) {
+        fs.readFile('./cities.json', 'utf-8', async (err, data) => {
+            if (err) console.log(err);
+            else {
+                let cities = JSON.parse(data);
+                await City.insertMany(cities);
+            }
+        })
+    }
+}
