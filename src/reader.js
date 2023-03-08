b781719ed36d25cc1517f6ef63c8a478e781381c
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-03-08 15:02:16 +0000

    Call sites in reader

--- src/reader.js
+++ src/reader.js
@@ -6,8 +6,22 @@ module.exports.GetAll = async () => {
     let cities = await City.find();
     let travels = await Travel.find();
     try {
-        await alibaba(travels, cities);
+        for (let i = 0; i < travels.length; i++) {
+            let travel = travels[i];
+            addTravelAttributes(travel, cities);
+            
+            await alibaba(travel);
+        }
     } catch (error) {
         logger(error);
     }
+}
+
+function addTravelAttributes(travel, cities) {
+    let origin = cities.find(city => city.id == travel.origin);
+    let destination = cities.find(city => city.id == travel.destination);
+    travel.origin_code = origin.code;
+    travel.destination_code = destination.code;
+    travel.origin_name = destination.name;
+    travel.destination_name = destination.name;
 }
